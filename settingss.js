3bb40ac061ef0148140c1616a09ce4411d9ef118
jv-x-orc <juvenslumbertobadio@gmail.com>
2025-08-03 15:24:10 -0400

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
-  OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
+  SESSION_ID: "malvin~13lHGLhT#nQZbySm42Jkp1AMyMW3naUz8ol85HEv6jE0w7ECz6lU ",  // add your Session Id make sure it starts with malvin~
+  OWNER_NUMBER: "50955335060", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
