5dd6a444338443b8793e5661a5f43105d87c1a9d
jv-x-orc <juvenslumbertobadio@gmail.com>
2025-08-13 22:17:06 -0400

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~I3klAZxJ#1vadjal8eL6kepENdotmOf-2zfeUVIWYWOm-9qCPRyw",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "50955335060", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
